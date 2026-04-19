2ef071ae3ed15bb305b1a9c3324e93c5e30c6c7a
Biancapetz2 <biancapetz@yahoo.com>
2026-04-19 20:30:48 -0300

    Ejercicio 1 - Variables y operaciones
    Ejercicio 1 practico, se creo el archivo index.html para incluir a app.js.
    Muestra resultado del contador y mensaje con datos.

---
+++ Ejercicios/c03-javasript/ej1-variables/app.js
@@ -0,0 +1,9 @@
+const nombre = "Bianca Petz";
+const edad = 21;
+const materia = "Desarrollo de Software";
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+let contador = 0;
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+console.log("El valor del contador es:", contador);
