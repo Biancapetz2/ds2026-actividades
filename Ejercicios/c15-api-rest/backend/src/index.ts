c790bd25466a5b735ebeb2243c37d26d22f5a740
Biancapetz2 <biancapetz@yahoo.com>
2026-08-03 22:22:27 -0300

    [AI-C15] Capas en Librería
    Se agrego /api/autores, actividad individual completa.

--- Ejercicios/c15-api-rest/backend/src/index.ts
+++ Ejercicios/c15-api-rest/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import libroRoutes from "./routes/libro.routes";
+import autorRoutes from "./routes/autor.routes";
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,7 @@ app.get("/", (_req, res) => {
 
 app.use("/api/libros", libroRoutes);
 
+app.use("/api/autores", autorRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
